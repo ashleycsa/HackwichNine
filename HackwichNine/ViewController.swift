fdbb81b700345caecc2e0c8b9d968d5203a4526c
Ashley Aurellano <ashley.aurellano@hotmail.com>
2020-04-14 15:11:34 -1000

    Completed Hackwich #9 and Problem Sets 1 and 2

--- HackwichNine/ViewController.swift
+++ HackwichNine/ViewController.swift
@@ -8,13 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+    
+{
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.myLabel.text=""
+        segmentedControl.selectedSegmentIndex = -1
     }
 
-
+    @IBAction func segmentedControlPressed(_ sender: Any)
+    {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            let userInputText=textField.text
+            self.myLabel.text=userInputText
+            
+        case 1:
+            let userInputText=textField.text
+            self.myLabel.text=userInputText
+        case 2:
+            let userInputText=textField.text
+            self.myLabel.text=userInputText
+        default:
+            break
+        }
+    }
+    
 }
 
